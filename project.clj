b109aa8ac20c7dddde5e8fa5013d81a7174e7b91
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-04-17 22:44:35 +0200

    Added project URL in project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject assert-json "0.1.0-SNAPSHOT"
   :description "Small library to test JSON objects"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/manuelp/assert-json"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
