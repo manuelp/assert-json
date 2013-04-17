1571168dbb7f0ed9dfb32d59bf897e5fdbae1760
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-04-17 22:50:56 +0200

    Better name for assert-property fn

--- src/assert_json/core.clj
+++ src/assert_json/core.clj
@@ -19,10 +19,11 @@
                    ": expected " value
                    ", found: " m-val)))
 
-(defn map-has? [m property value]
+(defn assert-property [m property value]
   (let [m-val (has-key m property)]
     (cond (= ::nothing-here m-val) (throw (property-not-found property))
           (not= (type m-val) (type value)) (throw (wrong-property-type property m-val value))
           (not= value m-val) (throw (wrong-property-value property m-val value))
           :default true)))
+
 
