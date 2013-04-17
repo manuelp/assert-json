8a34f7e8c216c6a347f3fc9482c7ab1482e69893
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-04-17 23:40:04 +0200

    Exceptions in a separate ns e docstrings.

--- src/assert_json/core.clj
+++ src/assert_json/core.clj
@@ -1,32 +1,32 @@
 (ns assert-json.core
-  (:use [cheshire.core :as json :only [parse-string]]))
+  (:use [cheshire.core :as json :only [parse-string]]
+        [assert-json.exceptions :only [property-not-found
+                                       wrong-property-type
+                                       wrong-property-value]]))
 
-(defn- has-key [m key]
+(defn- get-key 
+  "Get a key from a map, returning the value `::nothing-here` if no such key was found."
+  [m key]
   (get m key ::nothing-here))
 
-(defn- property-not-found [property]
-  (Exception. (str "Property not found: " property)))
-
-(defn- wrong-property-type
-  [property m-val value]
-  (Exception. (str "Wrong type value for property " 
-                   property ": expected " (type value) 
-                   ", found: " (type m-val))))
-
-(defn- wrong-property-value
-  [property m-val value]
-  (Exception. (str "Wrong value for property " property
-                   ": expected " value
-                   ", found: " m-val)))
-
-(defn- assert-property [m property value]
-  (let [m-val (has-key m property)]
+(defn assert-property 
+  "Assert that the map `m` contains the given `property` with the desired `value` and type."
+  [m property value]
+  (let [m-val (get-key m property)]
     (cond (= ::nothing-here m-val) (throw (property-not-found property))
           (not= (type m-val) (type value)) (throw (wrong-property-type property m-val value))
           (not= value m-val) (throw (wrong-property-value property m-val value))
           :default true)))
 
-(defmacro assert-json [json & body]
+(defmacro assert-json 
+  "Assert JSON properties (and corresponding values) in a convenient form.
+  
+  Example:
+  
+  (assert-json \"{\\\"prop\\\":1,\\\"other\\\":[1,2]}\"
+               \"prop\" 1)
+               \"other\" [1 2])"
+  [json & body]
   `(let [m# (parse-string ~json)] 
      (map (fn [c#] (assert-property m# (first c#) (second c#))) 
           (partition 2 (vector ~@body)))))
---
+++ src/assert_json/exceptions.clj
@@ -0,0 +1,20 @@
+(ns assert-json.exceptions)
+
+(defn property-not-found 
+  "Generate an `Exception` for a property that is not found in a JSON."
+  [property]
+  (Exception. (str "Property not found: " property)))
+
+(defn wrong-property-type
+  "Generate an `Exception` for a property with a value of the wrong type."
+  [property actual expected]
+  (Exception. (str "Wrong type value for property " 
+                   property ": expected " (type expected) 
+                   ", found: " (type actual))))
+
+(defn wrong-property-value
+  "Generate an `Exception` for a value of a property in a JSON which is different from the expected one."
+  [property m-val value]
+  (Exception. (str "Wrong value for property " property
+                   ": expected " value
+                   ", found: " m-val)))
