4385ada00b066cc391787c5e39f9998a30cd27f2
Manuel Paccagnella <manuel.paccagnella@gmail.com>
2013-04-18 12:54:10 +0200

    Configured project as mixed source Clojure/Java, Java for actual Java interface to the library

--- project.clj
+++ project.clj
@@ -3,5 +3,12 @@
   :url "https://github.com/manuelp/assert-json"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [cheshire "5.1.1"]])
+                 [cheshire "5.1.1"]]
+  
+  :source-paths ["src/clojure"]
+  :test-paths ["test/clojure"]
+  :java-source-paths ["src/java"]
+  :javac-options ["-target" "1.6" "-source" "1.6"]
+  :aot :all)
